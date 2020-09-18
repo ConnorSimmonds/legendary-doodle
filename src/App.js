84ff30062359f13d385e8799e54eefb4ff363a6e
Connor Simmonds <simmonconn@ecs.vuw.ac.nz>
2020-09-18 22:09:49 +1200

    Add in portfolio items to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
 
-function App() {
+const App = () => {
+  // Initialize state
+  const [ projects, setProjects ] = useState([]);
+
+  // Get projects
+  useEffect(() => {
+    fetch('/api/projects')
+          .then(res => res.json())
+          .then(projects => setProjects(projects));
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+          <h1>Welcome to my portfolio page!</h1>
+          <h3>My name is Connor Simmonds.<br></br>I'm a third year SWEN student at Victoria University of Wellington.</h3>
+
+          <h4>Below are a few projects I've made.</h4>
+
+          {
+              projects.length ? (
+                projects.map((project) => (
+                  <div style={{padding: 10}} key={project.name}>
+                  <Button 
+                      variant="contained"
+                      href={project.html_url}>
+                      {project.name}
+                  </Button>
+                  <p>{project.description}</p>
+              </div>
+              ))
+              ) : (
+                  <div>
+                      Loading projects..
+                  </div>
+              )
+          }
+      </div>
   );
 }
 
