cb4780e4fb8beef90e9fa298006a70ff1e9e4947
Connor Simmonds <connor.t.simmonds@gmail.com>
2021-05-26 18:02:54 +1200

    Add in an avatar next to introduction

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
 
 const App = () => {
   // Initialize state
@@ -17,11 +19,14 @@ const App = () => {
 
   return (
       <div className="App">
-
           <h1>Welcome to my portfolio page!</h1>
+          <div className="dialogue">
+          <Avatar alt="Connor Simmonds" src="/images/avatar/1.jpg" className="dialogueAvatar"/>
+          <div className="dialogueCard">
           <h3>My name is Connor Simmonds.<br></br>I'm a fourth year SWEN student at Victoria University of Wellington.</h3>
-
-          <h4>Below are a few projects I've made.</h4>
+          </div>
+          </div>
+          <h4>Below are a few GitHub projects.</h4>
           {
               projects.length ? (
                 projects.map((project) => (
